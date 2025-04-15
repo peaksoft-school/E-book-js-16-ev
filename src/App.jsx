0e1964aed4ffad96b1cd585fd3a346d0ef9b00c9
Aigul <saidillaeva saidillaevaaigul@gmail.com>
2025-04-15 16:58:24 +0600

    Add ReusableInput compones

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { ReusableInput } from './components/UI/Input/SearchField'
+import { ReusableInput } from './components/UI/Input/ReusableInput'
 
 const App = () => <h1>eBook-js-16-ev</h1>
 
