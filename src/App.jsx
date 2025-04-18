c76c5bd25e12041f04cf52ad340e0186334829e8
Nurebi Uraimov <nurebi2007@gmail.com>
2025-04-18 19:49:18 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,5 @@
-// import './App.css'
+import './App.css'
 
-// const App = () => <h1>eBook-js-16-ev</h1>
+const App = () => <h1>eBook-js-16-ev</h1>
 
-// export default App
-
-import React from 'react'
-import Chip from './components/UI/Chip'
-import { Icons } from './assets/icons'
-import { Box, styled } from '@mui/material'
-// import vec from "./assets/icons/svgs/vector.svg"
-
-const App = () => {
-   return (
-      <Box>
-         <Chip
-            label="Зарубежная литература"
-            onDelete={() => console.log('delete')}
-         />
-         <Chip
-            color="yellow"
-            label="color"
-            // deleteIcon={
-            // <img src={vec} style={{ width: 16, height: 16 } />
-            // }
-            onDelete={() => alert('hello1')}
-         />
-
-         <img src={Icons.x} alt="" />
-      </Box>
-   )
-}
 export default App
-
-const StyledBox = styled(Box)(() => ({}))
--- src/components/UI/Chip.jsx
+++ src/components/UI/Chip.jsx
@@ -1,121 +1,69 @@
-// import { Chip as MuiChip, styled } from '@mui/material'
-// import CloseIcon from '@mui/icons-material/Close' 
-// import closeicon from '../../assets/icons/svgs/crest.svg'
-
-// const Chip = ({
-//   label,
-//   onClick,
-//   onDelete,
-//   deleteIcon,
-//   icon,
-//   avatar,
-//   color = 'default',
-//   variant = 'filled',
-//   ...rest
-// }) => {
-//   const finalDeleteIcon = deleteIcon ?? <StyledIcon sx={{ width: 16,color: "black"}} />
-
-//   return (
-//     <StyleChip
-//       label={label}
-//       onClick={onClick}
-//       onDelete={onDelete}
-//       deleteIcon={onDelete ? finalDeleteIcon : undefined}
-//       icon={icon}
-//       avatar={avatar}
-//       color={color}
-//       variant={variant}
-//       clickable={!!onClick}
-//       {...rest}
-//     />
-//   )
-// }
-
-// export default Chip
-
-// const StyledIcon = styled(CloseIcon)`
-//    border-color: black;
-// `
-
-// const StyleChip = styled(MuiChip, {
-//    shouldForwardProp: (prop) => prop !== 'clickable',
-//  })(({ clickable }) => ({
-//    margin: '4px', 
-//    fontWeight: 500,
-//    cursor: clickable ? 'pointer' : 'default',
-//    border: '1px solid #c4c4c4',
-//    borderRadius: 0,
-//    backgroundColor: '#ffffff',
-//    '&:hover': {
-//      borderColor: '#f34901',
-//      color: '#f34901',
-//      backgroundColor: '#ffffff',
-//      '& .MuiChip-deleteIcon': {
-//        color: '#f34901',
-//      },
-//      '& .MuiChip-icon': {
-//        color: '#f34901',
-//      },
-//    },
-//  }))
 import { Chip as MuiChip, styled } from '@mui/material'
-import CloseIcon from '@mui/icons-material/Close' 
+import CloseIcon from '@mui/icons-material/Close'
+import { forwardRef } from 'react'
 
-const Chip = ({
-  label,
-  onClick,
-  onDelete,
-  deleteIcon,
-  icon,
-  avatar,
-  color = 'default',
-  variant = 'filled',
-  ...rest
-}) => {
-  const finalDeleteIcon = deleteIcon ?? <CloseIcon className="custom-delete-icon" />
+const Chip = forwardRef(
+   ({
+      label,
+      onClick,
+      onDelete,
+      deleteIcon,
+      icon,
+      avatar,
+      color = 'default',
+      variant = 'filled',
+      ...rest
+   }) => {
+      const finalDeleteIcon = deleteIcon ?? (
+         <CloseIcon className="custom-delete-icon" />
+      )
 
-  return (
-    <StyleChip
-      label={label}
-      onClick={onClick}
-      onDelete={onDelete}
-      deleteIcon={onDelete ? finalDeleteIcon : undefined}
-      icon={icon}
-      avatar={avatar}
-      color={color}
-      variant={variant}
-      clickable={!!onClick}
-      {...rest}
-    />
-  )
-}
+      return (
+         <StyleChip
+            label={label}
+            onClick={onClick}
+            onDelete={onDelete}
+            deleteIcon={onDelete ? finalDeleteIcon : undefined}
+            icon={icon}
+            avatar={avatar}
+            color={color}
+            variant={variant}
+            clickable={!!onClick}
+            {...rest}
+         />
+      )
+   }
+)
 
 export default Chip
 
 const StyleChip = styled(MuiChip, {
-  shouldForwardProp: (prop) => prop !== 'clickable',
+   shouldForwardProp: (prop) => prop !== 'clickable',
 })(({ clickable }) => ({
-  margin: '4px', 
-  fontWeight: 500,
-  boxShadow: "none",
-  cursor: clickable ? 'pointer' : 'default',
-  border: '1px solid #c4c4c4',
-  borderRadius: 0,
-  backgroundColor: '#ffffff',
-  '& .custom-delete-icon': {
-    color: 'black',
-    width: '14px'
-   //  transition: 'color 0.3s ease',
-  },
-  '&:hover': {
-    borderColor: '#f34901',
-    color: '#f34901',
-    backgroundColor: '#ffffff',
-    '& .custom-delete-icon': {
+   margin: '4px',
+   fontWeight: 500,
+   boxShadow: 'none',
+   cursor: clickable ? 'pointer' : 'default',
+   border: '1px solid #c4c4c4',
+   borderRadius: 0,
+   backgroundColor: '#ffffff',
+
+   '& .custom-delete-icon': {
+      color: 'black',
+      width: '14px',
+   },
+
+   '&:hover': {
+      borderColor: '#f34901',
       color: '#f34901',
-    },
-    '& .MuiChip-icon': {
-      color: '#f34901',
-    },
-  },
+      backgroundColor: '#ffffff',
+
+      '& .custom-delete-icon': {
+         color: '#f34901',
+      },
+
+      '& .MuiChip-icon': {
+         color: '#f34901',
+      },
+   },
 }))
