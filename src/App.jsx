b9246d3635c38543f8014dbbb66b2cfaf5b6ea14
Nurebi Uraimov <nurebi2007@gmail.com>
2025-04-18 20:09:38 +0600

    bekmirza

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,6 @@
-// import './App.css'
+import './App.css'
 
-// const App = () => <h1>eBook-js-16-ev</h1>
+const App = () => <h1>eBook-js-16-ev</h1>
 
-// export default App
-
-
-import { useState } from "react";
-import { Button, Typography } from "@mui/material";
-import Modal from "./components/UI/Modal";
-
-const App = () => {
-  const [open, setOpen] = useState(false);
-  return (
-    <>
-      <Button variant="contained" onClick={() => setOpen(true)}>
-        Open Modal
-      </Button>
-      <Modal open={open} handleClose={() => setOpen(false)}>
-        <Typography variant="h6">Reusable MUI Modal</Typography>
-        <Typography sx={{ mt: 2 }}>
-          hello world!
-        </Typography>
-        <Button onClick={() => setOpen(false)} sx={{ mt: 2 }}>
-          Close
-        </Button>
-      </Modal>
-    </>
-  );
-};
-export default App;
+export default App
+  
