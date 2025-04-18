b8dda0a3f9381bcb44f8f8f9607501defe64f14c
Aigul <saidillaeva saidillaevaaigul@gmail.com>
2025-04-18 19:58:50 +0600

    clean app

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,5 @@
-import React, { useState } from 'react'
-import Input from './components/UI/Input/Input'
+import './App.css'
 
-const App = () => {
-   const [value, setValue] = useState('')
-
-   const handleChange = (event) => {
-      setValue(event.target.value)
-   }
-
-   return (
-      <div>
-         <Input
-            type="search"
-            placeholder="Введите текст для поиска"
-            value={value}
-            onChange={handleChange}
-            label="Поиск"
-         />
-
-         <Input
-            type="password"
-            placeholder="Введите пароль"
-            value={value}
-            onChange={handleChange}
-            label="Пароль"
-         />
-         <Input
-            type="info"
-            placeholder="Информационное поле"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            label="ФИО"
-         />
-      </div>
-   )
-}
+const App = () => <h1>eBook-js-16-ev</h1>
 
 export default App
