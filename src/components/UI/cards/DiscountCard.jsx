e6ec1fe3016dabb88c73521832b3d4aba7b6b101
Aigul <saidillaeva saidillaevaaigul@gmail.com>
2025-05-01 23:40:40 +0600

    fixed cards

--- src/components/UI/cards/DiscountCard.jsx
+++ src/components/UI/cards/DiscountCard.jsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import { forwardRef } from 'react'
 import { styled } from '@mui/material/styles'
 import { Card, CardContent, CardMedia, Typography, Box } from '@mui/material'
 
