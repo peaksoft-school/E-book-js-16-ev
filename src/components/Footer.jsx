63333f936c2ec0659ca6414d9f3fc829e06556e7
Nurebi Uraimov <nurebi2007@gmail.com>
2025-05-07 12:02:08 +0600

    push

---
+++ src/components/Footer.jsx
@@ -0,0 +1,92 @@
+import { Box, Container, Typography, styled } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+
+const Footer = ({ variant = 'full' }) => {
+   return (
+      <FooterWrapper>
+         <FooterContainer maxWidth="lg" disableGutters variant={variant}>
+            <FooterColumn>
+               <LogoLink to="/">eBooK</LogoLink>
+               {variant === 'compact' && (
+                  <FooterLink>Политика конфиденциальности</FooterLink>
+               )}
+            </FooterColumn>
+
+            {variant === 'full' && (
+               <>
+                  <FooterColumn>
+                     <FooterLink>Жанры</FooterLink>
+                     <FooterLink>Аудиокниги</FooterLink>
+                     <FooterLink>Электронные книги</FooterLink>
+                  </FooterColumn>
+
+                  <FooterColumn>
+                     <FooterLink>Бестселлеры</FooterLink>
+                     <FooterLink>Промокоды</FooterLink>
+                     <FooterLink>Политика конфиденциальности</FooterLink>
+                  </FooterColumn>
+               </>
+            )}
+
+            <FooterColumn>
+               <FooterTitle>Свяжитесь с нами</FooterTitle>
+               <FooterLink>+996707123456</FooterLink>
+               <FooterLink>г. Бишкек ул. Исанова 45</FooterLink>
+            </FooterColumn>
+         </FooterContainer>
+      </FooterWrapper>
+   )
+}
+
+export default Footer
+
+const FooterWrapper = styled(Box)`
+   background-color: #1c1c1c;
+   color: white;
+   padding: 4.6875rem 14.1875rem 4.6875rem 5rem;
+   height: 16.25rem;
+`
+
+const FooterContainer = styled(Container, {
+   shouldForwardProp: (prop) => prop !== 'variant',
+})(({ variant }) => ({
+   display: 'flex',
+   justifyContent: 'space-between',
+   flexWrap: 'wrap',
+   margin: 0,
+}))
+
+const FooterColumn = styled(Box)`
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+`
+
+const LogoLink = styled(RouterLink)`
+   font-size: 1.875rem;
+   color: white;
+   text-decoration: none;
+   font-weight: bold;
+   margin-bottom: 1rem;
+
+   &:hover {
+      text-decoration: underline;
+   }
+`
+
+const FooterTitle = styled(Typography)`
+   font-size: 1.25rem;
+   font-weight: 500;
+   margin-bottom: 0.5rem;
+`
+
+const FooterLink = styled(RouterLink)`
+   font-size: 1rem;
+   color: white;
+   text-decoration: none;
+   line-height: 2rem;
+
+   &:hover {
+      text-decoration: underline;
+   }
+`
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Themes } from './components/Themes'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
    <StrictMode>
+      <BrowserRouter>
       <Themes>
          <App />
       </Themes>
+      </BrowserRouter>
    </StrictMode>
 )
