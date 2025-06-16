39be596059a16ce070128adde7e17900df12f0e3
Nurebi Uraimov <nurebi2007@gmail.com>
2025-06-16 16:37:36 +0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,6 @@
-// import './App.css'
-
-// const App = () => {}
-
-// export default App
-
-// import Applications from './pages/admin/Applications'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { loginSuccess } from './store/authSlice'
-// import Header from './layout/Header'
-// import ApplicationCard from './components/UI/cards/ApplicationCard'
+import './App.css'
 import AppRouter from './routes/AppRouter'
 
-const App = () => {
-   const dispatch = useDispatch()
-
-   useEffect(() => {
-      dispatch(
-         loginSuccess({
-            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImlkIjoxLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3NDg1OTc5MDYsImV4cCI6MTc0ODYzMzkwNn0.fO1E9eHV_kLWVMN7pLZ5ffGjX1bzM-rnywGp0DNaggI',
-            user: {
-               id: 1,
-               name: 'Admin',
-            },
-         })
-      )
-   }, [dispatch])
-
-   return (
-      <div>
-         {/* <Header /> */}
-         <AppRouter />
-         {/* <ApplicationCard/> */}
-      </div>
-   )
-}
+const App = () => <AppRouter />
 
 export default App
--- src/configs/axiosInstance.js
+++ src/configs/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://35.159.168.248'
+// const BASE_URL = 'http://35.159.168.248'
 
-// const BASE_URL = 'http://10.10.11.161:8080'
+const BASE_URL = 'http://10.10.11.161:8080'
 
 
 export const axiosInstance = axios.create({
@@ -31,7 +31,7 @@ axiosInstance.interceptors.request.use(
       console.log('👉 Токен:', token)
 
       if (token) {
-         updateConfig.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImlkIjoxLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3NTAwNjUwMTYsImV4cCI6MTc1MDEwMTAxNn0.fnwx2Hxf48t88cSxwTjXHy_yJSApnbhiKUHIloCzNh8`
+         updateConfig.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImlkIjoxLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3NTAwNjgxODIsImV4cCI6MTc1MDEwNDE4Mn0.WoXiK-ghEXl9WEh-iU1KGSH3KRpEOVUJ4TG4jBYeyMc`
       }
 
       return updateConfig
