9b20643e08698901a7e37e48f1329825d2b4657f
Aigul <saidillaeva saidillaevaaigul@gmail.com>
2025-06-28 19:16:11 +0600

    final

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,21 +1,24 @@
+import { getAuth } from 'firebase/auth'
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
-import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-   apiKey: 'AIzaSyBAKupIf8_kECBMKPb7B8msUlbFnC503oQ',
-   authDomain: 'ebook-js16.firebaseapp.com',
-   projectId: 'ebook-js16',
-   storageBucket: 'ebook-js16.firebasestorage.app',
-   messagingSenderId: '279474076115',
-   appId: '1:279474076115:web:b8c0b96f8972153cab5726',
-   measurementId: 'G-QY0SSM6ETQ',
+   apiKey: 'AIzaSyBziE5CvfXSrUcCarzH4pyIqkrjUwgtJaI',
+   authDomain: 'ebook-b16-ev.firebaseapp.com',
+   projectId: 'ebook-b16-ev',
+   storageBucket: 'ebook-b16-ev.firebasestorage.app',
+   messagingSenderId: '462607613580',
+   appId: '1:462607613580:web:f8bc740981287922b085a9',
+   measurementId: 'G-VJW3Y12MCR',
 }
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig)
-
-const auth = getAuth(app)
-
 const analytics = getAnalytics(app)
+const auth = getAuth(app)
 
 export { auth, app, analytics }
